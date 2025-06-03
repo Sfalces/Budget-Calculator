367048aa4463e495172760135a9e01a4b3257eb6
Sergi Falces <90058387+Sfalces@users.noreply.github.com>
2025-06-03 22:54:24 +0200

    add and test form (#2)
    Co-authored-by: Sergi Falces <sergifalces@MacBook-Air-de-Sergi.local>

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,20 @@ const { createDefaultPreset } = require('ts-jest')
 
 const tsJestTransformCfg = createDefaultPreset().transform
 
-/** @type {import("jest").Config} **/
-export default {
-  preset: 'ts-jest',
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    ...tsJestTransformCfg
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
+    '^@(.*)/(.*)$': '<rootDir>/node_modules/@$1/$2',
+    '^core/(.*)$': '<rootDir>/src/core/$1',
+    '^ui/(.*)$': '<rootDir>/src/ui/$1',
+    '^_di/(.*)$': '<rootDir>/src/_di/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   }
 }
---
+++ src/ui/BudgetFrom/BudgetForm.tsx
@@ -0,0 +1,36 @@
+import { useState, ChangeEvent, useMemo } from 'react'
+
+export const BudgetForm = () => {
+  const [budget, setBudget] = useState(0)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber)
+  }
+
+  const isValid = isNaN(budget) || budget <= 0
+
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+          Define tu Presupuesto
+        </label>
+        <input
+          id="budget"
+          type="number"
+          className="w-full bg-white border border-gray200 p-2"
+          placeholder="Define tu presupuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        value="Definir Presupuesto"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+        disabled={isValid}
+      />
+    </form>
+  )
+}
