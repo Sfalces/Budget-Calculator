b54f299bda2e7928f62867d33fee406464aeb28e
Sergi Falces <sergifalces@MacBook-Air-de-Sergi.local>
2025-06-03 14:41:18 +0200

    add jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Control de gastos</h1>
+      <h1 className="text-8xl">Control de gastos</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
